593e75572910d84cbee6cd9a86f7fe98b0d52c2a
zhangwu <zhangwu@hyc.cn>
2019-01-03 09:30:43 +0700

    Initial Commit

---
+++ Hoping/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hoping
+//
+//  Created by hyc on 2019/1/3.
+//  Copyright © 2019 HYC. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hoping/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hoping
+//
+//  Created by hyc on 2019/1/3.
+//  Copyright © 2019 HYC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
